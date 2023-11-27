873a3d2540f61a4e6e23be51d9cc423731d1aaa7
Alexander Parfionov <parfion.alexander@yandex.ru>
2023-11-27 19:29:09 +0300

    add Promo

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,11 +1,13 @@
 import './App.css';
 import '../../vendor/normalize.css'
 import Header from '../Header/Header';
+import Promo from '../Promo/Promo';
 
 function App() {
   return (
     <div className="app">
       <Header />
+      <Promo />
     </div>
   );
 }
---
+++ src/components/Promo/Promo.jsx
@@ -0,0 +1,14 @@
+import './Promo.css';
+
+function Promo(params) {
+  return(
+    <div className="promo">
+      <div className='promo__content'>
+        <h1 className='promo__title'>Учебный проект студента факультета Веб-разработки.</h1>
+        <div className='promo__img'></div>
+      </div>
+    </div>
+  )
+};
+
+export default Promo;
