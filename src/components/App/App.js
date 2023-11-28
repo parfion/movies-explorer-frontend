ac00eed2cf5703e1c4c639dd4c454bbeb7347072
Alexander Parfionov <parfion.alexander@yandex.ru>
2023-11-28 16:15:40 +0300

    add about-project

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -2,12 +2,14 @@ import './App.css';
 import '../../vendor/normalize.css'
 import Header from '../Header/Header';
 import Promo from '../Promo/Promo';
+import AboutProject from '../AboutProject/AboutProject';
 
 function App() {
   return (
     <div className="app">
       <Header />
       <Promo />
+      <AboutProject />
     </div>
   );
 }
