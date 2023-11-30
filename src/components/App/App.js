169a2d7b769fe7f20e0cdecba31ec665a8ea469c
Alexander Parfionov <parfion.alexander@yandex.ru>
2023-11-30 15:48:54 +0300

    add Main

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -6,7 +6,7 @@ import AboutProject from '../AboutProject/AboutProject';
 import Techs from '../Techs/Techs';
 import AboutMe from '../AboutMe/AboutMe';
 import Portfolio from '../Portfolio/Portfolio';
-import Footet from '../Footer/Footer';
+import Footer from '../Footer/Footer';
 
 function App() {
   return (
@@ -17,7 +17,7 @@ function App() {
       <Techs />
       <AboutMe />
       <Portfolio />
-      <Footet />
+      <Footer />
     </div>
   );
 }
